46ab7cb2011011e38c7687beee8fc084c933c540
Gabe D <gjdeleon@hotmail.ca>
2018-12-16 19:44:22 -0500

    Split getWeather logic into two separate functions.
    API Call is now own function, and processing data is own function.

--- weatherchecker/src/App.js
+++ weatherchecker/src/App.js
@@ -27,13 +27,14 @@ class App extends React.Component {
       dayDescription: [],
     };
     this.getWeather = this.getWeather.bind(this);
+    this.updateWeather = this.updateWeather.bind(this);
   };
 
   //run call to openweather maps and receive forecast after button press
   async getWeather(e) {
     //prevent page reload (default behaviour) on form submit
     e.preventDefault();
-
+    //take input from formJS
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
     //api call for current forecast
@@ -43,27 +44,29 @@ class App extends React.Component {
     const call5Day = await fetch (`http://api.openweathermap.org/data/2.5/forecast?units=metric&q=${city},${country}&APPID=${API_KEY}`);
     const data5Day = await call5Day.json();
 
-    //check if input exists from form
-    if(city && country){
-      //if successful api call, object should exist
-      if(dataToday.main){
-        let counter = 0;
+    this.updateWeather(city, country, dataToday, data5Day);
+  };
+
+  //process data taken from API call
+  updateWeather(city, country, dataToday, data5Day){
+    if(city && country){//check if input exists from form
+      if(dataToday.main){ //if successful api call, start filling objects to push to forecast JS
         let dateDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
         let next5Days = [];
         let week5Days = [];
         let temp5Days = [];
         let weather5Days = [];
         let desc5Days = [];
-
         //need to get date when checking for future dates on 5day forecast
         let now = new Date();
         let today = now.getFullYear()+'-'+(now.getMonth()+1)+'-'+(now.getDate());
         //marks if todays date already found when searching object
         let todayNotFound = true;
+        let futureDaysFound = 0;
 
         for(let i=0; i<data5Day.list.length;i++){
           //break loop if found 5 dates
-          if(counter === 5){
+          if(futureDaysFound === 5){
             break;
           }
           if(i < 5){
@@ -82,15 +85,14 @@ class App extends React.Component {
             }
           }
           //take found day and get weather info from corresponding JSON string
-          if(data5Day.list[i].dt_txt.startsWith(next5Days[counter])){
+          if(data5Day.list[i].dt_txt.startsWith(next5Days[futureDaysFound])){
             temp5Days.push(data5Day.list[i].main.temp);
             weather5Days.push(data5Day.list[i].weather[0].icon);
             desc5Days.push(data5Day.list[i].weather[0].description);
-            counter++;
+            futureDaysFound++;
           }
         }
-
-        //after set state of component to hold found weather data
+        //set state of component to hold found weather data
         this.setState({
           temperature: dataToday.main.temp,
           humidity: dataToday.main.humidity,
@@ -107,7 +109,7 @@ class App extends React.Component {
           status: ' ',
         });
       }
-      else{
+      else{ //if cannot find city, empty state
         this.setState({
           temperature: null,
           humidity: null,
@@ -125,7 +127,7 @@ class App extends React.Component {
         });
       }
     }
-    else {
+    else { //if values missing from form, empty state
       this.setState({
         temperature: null,
         humidity: null,
@@ -144,6 +146,7 @@ class App extends React.Component {
     }
   };
 
+  //render UI in browser
   render() {
     return(
       <div>
